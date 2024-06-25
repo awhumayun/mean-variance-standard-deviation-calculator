0b4696db9f001d67597826e99750345a046fba3c
awhumayun <abd.humayun@gmail.com>
2024-06-25 20:50:03 +0000

    Rename flat to flattened

--- mean_var_std.py
+++ mean_var_std.py
@@ -8,33 +8,33 @@ def calculate(list):
     
     mean_axis1 = np.mean(matrix, axis=0).tolist()
     mean_axis2 = np.mean(matrix, axis=1).tolist()
-    mean_flat = np.mean(matrix).tolist()
+    mean_flattened = np.mean(matrix).tolist()
     
     variance_axis1 = np.var(matrix, axis=0).tolist()
     variance_axis2 = np.var(matrix, axis=1).tolist()
-    variance_flat = np.var(matrix).tolist()
+    variance_flattened = np.var(matrix).tolist()
     
     std_axis1 = np.std(matrix, axis=0).tolist()
     std_axis2 = np.std(matrix, axis=1).tolist()
-    std_flat = np.std(matrix).tolist()
+    std_flattened = np.std(matrix).tolist()
     
     max_axis1 = np.max(matrix, axis=0).tolist()
     max_axis2 = np.max(matrix, axis=1).tolist()
-    max_flat = np.max(matrix).tolist()
+    max_flattened = np.max(matrix).tolist()
     
     min_axis1 = np.min(matrix, axis=0).tolist()
     min_axis2 = np.min(matrix, axis=1).tolist()
-    min_flat = np.min(matrix).tolist()
+    min_flattened = np.min(matrix).tolist()
     
     sum_axis1 = np.sum(matrix, axis=0).tolist()
     sum_axis2 = np.sum(matrix, axis=1).tolist()
-    sum_flat = np.sum(matrix).tolist()
+    sum_flattened = np.sum(matrix).tolist()
     
     return {
-        'mean': [mean_axis1, mean_axis2, mean_flat],
-        'variance': [variance_axis1, variance_axis2, variance_flat],
-        'standard deviation': [std_axis1, std_axis2, std_flat],
-        'max': [max_axis1, max_axis2, max_flat],
-        'min': [min_axis1, min_axis2, min_flat],
-        'sum': [sum_axis1, sum_axis2, sum_flat]
+        'mean': [mean_axis1, mean_axis2, mean_flattened],
+        'variance': [variance_axis1, variance_axis2, variance_flattened],
+        'standard deviation': [std_axis1, std_axis2, std_flattened],
+        'max': [max_axis1, max_axis2, max_flattened],
+        'min': [min_axis1, min_axis2, min_flattened],
+        'sum': [sum_axis1, sum_axis2, sum_flattened]
     }
